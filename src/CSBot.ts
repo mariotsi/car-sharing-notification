dee5dcbbdbf8aefea0d6722da44803436cfefaeb
Simone Mariotti <simone.mariotti@piksel.com>
2017-08-01 10:53:39 +0200

    removed Moment, now getting the botName

--- src/CSBot.ts
+++ src/CSBot.ts
@@ -1,27 +1,47 @@
 import * as TeleBot from 'telebot';
-import * as Moment from 'moment';
+import * as distanceInWords from 'date-fns/distance_in_words';
+
 class Bot {
   private updateIds: (callback: () => void) => void;
   private bot: TeleBot;
+  private name: any;
+
   constructor(updateIds: (callback: any) => void, token: string = process.env['TELEGRAM:token']) {
     this.updateIds = updateIds;
     this.bot = new TeleBot(token);
     this.bot.connect();
-    this.sendMessage('Ok, I\'m fine after restart 🎉');
+    this.getName();
+
     this.bot.on('/echo', (msg) => {
       this.sendMessage(`Echo un cazzo!`, msg.from.id);
     });
 
     this.bot.on('/uptime', (msg) => {
-      this.sendMessage(`I'm online since ${Moment.duration(process.uptime(), 'seconds').humanize()}`, msg.from.id);
+      this.sendMessage(
+        `I'm online since ${distanceInWords(new Date(), +new Date() - Math.floor(process.uptime()) * 1000, {
+          includeSeconds: true,
+        })}`,
+        msg.from.id
+      );
     });
     this.bot.on('/update', (msg) => {
       this.updateIds(() => this.sendMessage(`Sincronizzazione con DB avvenuta`, msg.from.id));
+    });
+    this.bot.on(/^\/start (.*)$/, (msg, q) => {
+      this.sendMessage(q.match[1]);
     });
   }
 
   sendMessage(message: string, toId = process.env['TELEGRAM:clientId'], options = {parseMode: 'HTML'}) {
     this.bot.sendMessage(toId, message, options);
   }
+
+  async getName() {
+    this.name = await this.bot.getMe();
+    this.sendMessage(
+      `Ok, I\'m fine after restart 🎉 \n\nBot name: ${this.name.first_name}\nBot username: ${this.name.username}`
+    );
+  }
 }
+
 export default Bot;
